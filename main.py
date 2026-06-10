5a63a694855426575c9512c260070cbc68206a3f
Arthur Noreika <arthurnoreika11@live.com>
2026-06-10 11:12:43 +0100

    Refactor convert_to_markdown function: correct variable naming for input and output paths

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from pathlib import Path
-from parser.reader import FileTypeError, read_docx, read_pdf
-from parser.mdconverter import docx_to_markdown, pdf_to_markdown
+from workers.reader import FileTypeError, read_docx, read_pdf
+from workers.mdconverter import docx_to_markdown, pdf_to_markdown
 
 def convert_to_markdown(input_path: str, output_path: str) -> None:
     ext = Path(input_path).suffix.lower()
     try:
         if ext == ".docx":
-            content = read_docx(inputPath)
+            content = read_docx(input_path)
             mdContent = docx_to_markdown(content)
         elif ext == ".pdf":
-            content = read_pdf(inputPath)
+            content = read_pdf(input_path)
             mdContent = pdf_to_markdown(content)
         else:
             print(f"Unsupported file type: {ext}")
             return
 
-        with open(outputPath, "w", encoding="utf-8") as f:
+        with open(output_path, "w", encoding="utf-8") as f:
             f.write(mdContent)
 
-        print(f"Markdown file created at: {outputPath}")
+        print(f"Markdown file created at: {output_path}")
     except FileTypeError:
         print(f"Unsupported file type: {ext}")
     except FileNotFoundError:
-        print(f"File not found: {inputPath}")
+        print(f"File not found: {input_path}")
     except Exception as e:
         print(f"Error processing file: {e}")
